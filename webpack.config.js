7b7fa649fb8c56cd7c6cf3bddd76b0eb3d3a591f
wuxue <wuxue@finchina.com>
2025-03-23 20:14:58 +0800

    build: 项目webpack初始配置

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const webpack = require("webpack"); // 用于访问内置插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].[hash:8].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name].[hash:8].[ext]",
+        },
+        // TODO,不知道怎么正确配置
+        // parser: {
+        //   dataUrlCondition: {
+        //     maxSize: 10 * 1024,
+        //   },
+        // },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
+};
