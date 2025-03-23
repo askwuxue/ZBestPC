bfafc90ef4f156ccf92efb4e5f399e02fe070cd1
wuxue <wuxue@finchina.com>
2025-03-23 22:27:27 +0800

    build: 为页面单独打包chunk文件, 启动devServer

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,27 @@ const path = require("path");
 
 const webpack = require("webpack"); // 用于访问内置插件
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
+    login: "./src/js/login.js",
   },
   output: {
     filename: "[name].[hash:8].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    open: true,
+    hot: true,
+    port: 8080,
   },
   module: {
     rules: [
@@ -35,13 +46,23 @@ module.exports = {
     ],
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./src/img"),
+          to: path.resolve(__dirname, "./dist/img"),
+        },
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
+      chunks: ["index"],
     }),
     new HtmlWebpackPlugin({
-      template: "./src/login.html",
+      template: "./src/pages/login.html",
       filename: "login.html",
+      chunks: ["login"],
     }),
     new webpack.ProvidePlugin({
       $: "jquery",
